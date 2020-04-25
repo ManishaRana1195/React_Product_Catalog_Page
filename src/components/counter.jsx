5203cc85b8e4f6eaaadaa69712eb4f9b7eb981f4
ManishaRana1195 <manisha1195@gmail.com>
2020-04-25 15:24:29 -0500

    rendering based on conditions

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,22 +1,30 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = { count: 0 };
-  render() {
+  state = { count: 0, tags: ["tags1", "tags2", "tags3"] };
 
+  render() {
     return (
       <div>
-        <span className={this.getBadgeClasses()}>{this.state.count}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        {
+            /* <span className={this.getBadgeClasses()}>{this.state.count}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button> */
+        this.renderTagsList()
+        }
       </div>
     );
   }
 
-    getBadgeClasses() {
-        let spanClass = "badge m-2 badge-";
-        spanClass += this.state.count === 0 ? "warning" : "primary";
-        return spanClass;
+
+  renderTagsList() {
+        if ( this.state.tags.length == 0) return <p>No tags!!</p>
+        return <ul> {this.state.tags.map(tag => <li key={tag}>{tag}</li>)} </ul>;
     }
+//   getBadgeClasses() {
+//     let spanClass = "badge m-2 badge-";
+//     spanClass += this.state.count === 0 ? "warning" : "primary";
+//     return spanClass;
+//   }
 }
 
 export default Counter;
