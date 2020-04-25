958c7c298f030b1df1779d7d9e4ee8140acab5ec
ManishaRana1195 <manisha1195@gmail.com>
2020-04-25 16:42:54 -0500

    passing data from parent to children component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,30 +1,37 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = { count: 0, tags: ["tags1", "tags2", "tags3"] };
+  state = { value: this.props.value };
+
+  getBadgeClasses() {
+    let spanClass = "badge m-2 badge-";
+    spanClass += this.state.value === 0 ? "warning" : "primary";
+    return spanClass;
+  }
+
+  handleIncrement = dummyValue => {
+    /* in javascript, "this" can be different values based on 
+      how is parent method invokee. here this is undefined, to 
+      prevent that we use arrow notation = () => 
+      state in React needs to be manually updated. React 
+      wont read the updated state automatically*/
+    this.setState({ value: this.state.value + 1 });
+    console.log(dummyValue);
+  };
 
   render() {
     return (
       <div>
-        {
-            /* <span className={this.getBadgeClasses()}>{this.state.count}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button> */
-        this.renderTagsList()
-        }
+        <span className={this.getBadgeClasses()}>{this.state.value}</span>
+        <button
+          onClick={() => this.handleIncrement(5)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
   }
-
-
-  renderTagsList() {
-        if ( this.state.tags.length == 0) return <p>No tags!!</p>
-        return <ul> {this.state.tags.map(tag => <li key={tag}>{tag}</li>)} </ul>;
-    }
-//   getBadgeClasses() {
-//     let spanClass = "badge m-2 badge-";
-//     spanClass += this.state.count === 0 ? "warning" : "primary";
-//     return spanClass;
-//   }
 }
 
 export default Counter;
